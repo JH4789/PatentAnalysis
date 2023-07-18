ebd368a9f9c89b25b7acace10dca22c61b49518e
Jayden Huang <jayden.cx.huang@gmail.com>
2023-07-18 09:18:28 -0700

    FirstCommit

---
+++ Analysis/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+def main():
+    print("Hello World")
+    df = pd.read_csv("cited_by.csv")
+#    counts = df['Pub'].value_counts().to_frame()
+#    print(counts[20:30])
+main()
